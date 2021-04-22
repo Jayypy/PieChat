0fd51597625ca78703d14d76c86b735640f7afbb
Jonas <68991664+Jayypy@users.noreply.github.com>
2021-04-22 12:13:35 +0200

    Update Server.py

--- Server.py
+++ Server.py
@@ -2,22 +2,18 @@ import socket
 from threading import Thread
 from clint.textui import colored
 
-def makesocket():
-    s = socket.socket()                                             
-    port = int(input("Port: "))  
-    name = input("Your Name: ")                                   
-    s.bind(('', port))                                                
-    s.listen(5)                                                     
-    c, addr = s.accept()   
-    c.send(name.encode())                                                
-    print ("Connection found",addr)
-    cname = c.recv(1024).decode()
-    print("ClientName: " + colored.red(cname) + "\n")
+s = socket.socket()                                             
+port = int(input("Port: "))  
+name = input("Your Name: ")                                   
+s.bind(('', port))                                                
+s.listen(5)                                                     
+c, addr = s.accept()   
+c.send(name.encode())                                                
+print ("Connection found",addr)
+cname = c.recv(1024).decode()
+print("ClientName: " + colored.red(cname) + "\n") 
 
-    Thread(target=receive, args={c, cname}).start()
-    Thread(target=send, args=c).start()
-
-def receive(c, cname):
+def receive():
     while 1:
         rcvdData = c.recv(1024).decode()                                    
         print (colored.red("\n" + cname + ": "), rcvdData)
@@ -29,7 +25,7 @@ def receive(c, cname):
             break
     c.close()
 
-def send(c):
+def send():
     while 1:
         sendData = input()                                         
         c.send(sendData.encode())                                         
@@ -41,7 +37,8 @@ def send(c):
     c.close()
 
 def main():
-    makesocket()
+    Thread(target=receive).start()
+    Thread(target=send).start()
 
 if __name__ == '__main__':
     main()
