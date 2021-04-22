10a54a8fa3dee3b18ae05de8049a03a56704ae23
Jonas <68991664+Jayypy@users.noreply.github.com>
2021-04-22 10:47:33 +0200

    Update Client.py

--- Client.py
+++ Client.py
@@ -2,37 +2,44 @@ import socket
 from threading import Thread
 from clint.textui import colored
 
-s = socket.socket()                               
-server_ip = input("Server IP: ")                    
-server_port = int(input("Server Port: "))            
-s.connect((server_ip, server_port))          
+def makesocket():
+    s = socket.socket()          
+    name = input("Your Name: ")                   
+    server_ip = input("Server IP: ")                    
+    server_port = int(input("Server Port: "))            
+    s.connect((server_ip, server_port))
+    cname = s.send(name.encode())     
+    sname = s.recv(1024).decode()
+    print("Server name: " + colored.red(sname) + "\n")
 
-def receive():
+    Thread(target=receive, args={s, sname}).start()
+    Thread(target=send, args=s).start()
+
+def receive(s, sname):
     while 1:
         rcvdData = s.recv(1024).decode()
-        print (colored.red("\nFrom: "),rcvdData)
+        print (colored.red("\n" + sname + ": "),rcvdData)
         print("")
         if rcvdData == "Bye" or rcvdData == "bye":
-            print("\n+++++++++++++++++++")
-            print(colored.blue("Connection ended"))    
-            print("+++++++++++++++++++\n")
+            print("\n+++++++++++++++++++++++++++++++++++")
+            print(colored.blue("Connection ended (Press Enter to exit)"))    
+            print("+++++++++++++++++++++++++++++++++++\n")
             break
     s.close()
 
-def send():
+def send(s):
     while 1:
         str = input()                            
         s.send(str.encode())                            
         if(str == "Bye" or str == "bye"):  
-            print("\n+++++++++++++++++++")
+            print("\n++++++++++++++++++++++")
             print(colored.blue("Connection ended"))    
-            print("+++++++++++++++++++\n")          
+            print("++++++++++++++++++++++\n")         
             break
     s.close()
 
 def main():
-    Thread(target=receive).start()
-    Thread(target=send).start()
+    makesocket()
 
 if __name__ == '__main__':
     main()
